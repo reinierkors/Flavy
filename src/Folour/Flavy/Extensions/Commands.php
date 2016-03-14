5dfb369a1c9ae0278347e3943b724986310e6c92
Vadim Bova <folour@gmail.com>
2016-03-14 22:38:35 +0200

    fix

--- src/Folour/Flavy/Extensions/Commands.php
+++ src/Folour/Flavy/Extensions/Commands.php
@@ -43,7 +43,7 @@ abstract class Commands
         ],
 
         'get_thumbnails' => [
-            'cmd' => '%s -i "%s" -vf fps=fps=1/%d -vframes %d "%s"'
+            'cmd' => '%s -i "%s" -vf fps=1/%d -vframes %d "%s"'
         ]
     ];
 
